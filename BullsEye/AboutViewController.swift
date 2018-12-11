2bf91da93966d0c66918f3bef72f71aaba46e3cd
YZune <windowsyangzeng@hotmail.com>
2018-12-11 21:38:00 +0800

    使用自适应布局

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by yzune on 2018/12/11.
+//  Copyright © 2018 yzune. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
